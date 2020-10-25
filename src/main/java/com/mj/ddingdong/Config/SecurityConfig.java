fc25ea6e808e4b06cddfc7a7da53a543cd058503
kimyubi <ieieie0419@gmail.com>
2020-10-25 15:57:33 +0900

    로그인 상태 유지

--- src/main/java/com/mj/ddingdong/Config/SecurityConfig.java
+++ src/main/java/com/mj/ddingdong/Config/SecurityConfig.java
@@ -1,17 +1,26 @@
 package com.mj.ddingdong.Config;
 
+import com.mj.ddingdong.account.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    private final AccountService accountService;
+    private final DataSource dataSource;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -32,5 +41,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         http.logout()
                 .logoutUrl("/log-out")
                 .logoutSuccessUrl("/");
+
+        http.rememberMe()
+                .userDetailsService(accountService)
+                .tokenRepository(tokenRepository());
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
     }
 }
